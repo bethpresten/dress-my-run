d8f986619e30f472ae8211af821b861007dd1676
Beth Presten <bethpresten@gmail.com>
2021-03-06 19:12:18 -0500

    added sticky footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-import Navbar from "../src/components/Navbar/Navbar"
+import Navbar from "../src/components/Navbar/Navbar";
+import Footer from "../src/components/Footer/Footer"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Footer />
     </div>
   );
 }
